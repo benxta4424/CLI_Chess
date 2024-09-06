9cb13b00dba8bd138fc05de6b26ad8eacc59dabf
benxta4424 <benxtaben@gmail.com>
2024-09-06 13:50:09 +0100

    creating the grid jsut like last time

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,31 @@
+require 'colorize'
+
 class Board
+    attr_accessor :board, :first_color, :second_color
+
     def initialize
-        @board=Array.new(7){Array.new(7)}        
+        @board=Array.new(7){Array.new(7)}
+        @first_color=:light_white
+        @second_color=:green
+    end
+
+    def is_even?(ind_one,ind_two)
+        (ind_one+ind_two).even?
+    end
+
+    def draw_board
+        board.each_with_index do |row,row_ind|
+            row.each_with_index do |col,col_ind|
+                if is_even?(row_ind,col_ind)
+                    row[col_ind]="   ".colorize(background: first_color)
+                else
+                    row[col_ind]="   ".colorize(background: second_color)
+                end
+            end
+        end
+    end
+
+    def print_board
+        board.each {|row| puts row.join}
     end
 end
