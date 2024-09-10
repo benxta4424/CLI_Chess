994262ae5e8516e848fd450ae3f2e106171803d5
benxta4424 <benxtaben@gmail.com>
2024-09-10 23:04:30 +0100

    a piece's possible moves implemented in the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,103 +1,101 @@
-require 'colorize'
-require './lib/rook'
+require "colorize"
+require "./lib/rook"
 
 class Board
-    attr_accessor :board, :first_color, :second_color
+  attr_accessor :board, :first_color, :second_color
 
-    def initialize
-        @board=Array.new(8){Array.new(8)}
-        @first_color=:light_white
-        @second_color=:green
-        @pieces=Array.new(8){Array.new(8)}
+  def initialize
+    @board = Array.new(8) { Array.new(8) }
+    @first_color = :light_white
+    @second_color = :green
+    @pieces = Array.new(8) { Array.new(8) }
+  end
+
+  def draw_board
+    board.each_with_index do |row, row_ind|
+      row.each_with_index do |_col, col_ind|
+        row[col_ind] = if is_even?(row_ind, col_ind)
+                         "   ".colorize(background: first_color)
+                       else
+                         "   ".colorize(background: second_color)
+                       end
+      end
     end
+  end
 
+  def print_board
+    board.each { |row| puts row.join }
+  end
 
-    def draw_board
-        board.each_with_index do |row,row_ind|
-            row.each_with_index do |col,col_ind|
-                if is_even?(row_ind,col_ind)
-                    row[col_ind]="   ".colorize(background: first_color)
-                else
-                    row[col_ind]="   ".colorize(background: second_color)
-                end
-            end
-        end
+  def pieces
+    @pieces.each do |items|
+      puts items.join
     end
+  end
 
-    def print_board
-        board.each {|row| puts row.join}
+  def is_even?(ind_one, ind_two)
+    (ind_one + ind_two).even?
+  end
+
+  def color_piece(ind_one, ind_two)
+    is_even?(ind_one, ind_two) ? first_color : second_color
+  end
+
+  def is_legal?(ind_one, ind_two)
+    if ind_one.between?(0, 7) && ind_two.between?(0, 7)
+      true
+    elsif !ind_one.between?(0, 7) || !ind_two.between?(0, 7)
+      false
+    else
+      raise ArgumentError
     end
+  end
 
+  def colorize_square(piece, first_ind, second_ind)
+    piece.colorize(background: color_piece(first_ind, second_ind))
+  end
 
-    def pieces
-        @pieces.each do |items|
-            puts items.join
-        end
+  def add_pieces(piece_class, piece_type, add_x, add_y)
+    # black_rooks
+    first_black_rook = piece_class.new(piece_type)
+    @pieces[add_x][add_y] = first_black_rook
+    board[add_x][add_y] = first_black_rook.symbol.colorize(background: color_piece(add_x, add_y))
+  end
+
+  def move_pieces(old_position, new_position)
+    old_x = old_position[0]
+    old_y = old_position[1]
+
+    new_x = new_position[0]
+    new_y = new_position[1]
+
+    # move the rook on the board
+    current_piece = @pieces[old_x][old_y]
+    current_piece.move_front(board, [old_x, old_y], [new_x, new_y])
+    board[new_x][new_y] = current_piece.symbol.colorize(background: color_piece(new_x, new_y))
+
+    # move the index in the pieces array
+    @pieces[old_x][old_y] = nil
+    @pieces[new_x][new_y] = current_piece
+
+    # deleting the old rook from the board
+    delete_old_pieces_after_moving(old_x, old_y)
+  end
+
+  def delete_old_pieces_after_moving(old_x, old_y)
+    # deleting the old rook from the board
+    board[old_x][old_y] = "   ".colorize(background: color_piece(old_x, old_y))
+  end
+
+  def piece_possible_moves(x_choice,y_choice)
+    piece=@pieces[x_choice][y_choice]
+
+    possible_moves=piece.possible_moves(x_choice,y_choice)
+  end
+
+  def pieces
+    @pieces.each do |items|
+      puts items.join
     end
-
-    def is_even?(ind_one,ind_two)
-        (ind_one+ind_two).even?
-    end
-
-    def color_piece(ind_one,ind_two)
-        is_even?(ind_one,ind_two) ? first_color : second_color
-    end
-
-    def is_legal?(ind_one,ind_two)
-
-        if ind_one.between?(0,7) && ind_two.between?(0,7)
-            return true
-        elsif !ind_one.between?(0,7) || !ind_two.between?(0,7)
-            return false
-        else
-            raise ArgumentError
-        end
-    end
-
-    def colorize_square(piece,first_ind,second_ind)
-        piece.colorize(background: color_piece(first_ind,second_ind))
-    end
-
-    def add_pieces(add_x,add_y)
-        #black_rooks
-        first_black_rook=Rook.new('black')
-        @pieces[add_x][add_y]=first_black_rook
-        board[add_x][add_y]= first_black_rook.symbol.colorize(background: color_piece(add_x,add_y))
-
-    end
-
-    def move_pieces(old_position,new_position)
-
-        old_x=old_position[0]
-        old_y=old_position[1]
-
-        new_x=new_position[0]
-        new_y=new_position[1]
-
-        #move the rook on the board
-        first_black_rook=@pieces[old_x][old_y]
-        first_black_rook.move_front(board,[old_x,old_y],[new_x,new_y])
-        board[new_x][new_y]= first_black_rook.symbol.colorize(background: color_piece(new_x,new_y))
-
-        
-
-        #move the index in the pieces array
-        @pieces[old_x][old_y]=nil
-        @pieces[new_x][new_y]=first_black_rook
-
-        #deleting the old rook from the board
-        delete_old_pieces_after_moving(old_x,old_y)
-    end
-    
-    def delete_old_pieces_after_moving(old_x,old_y)
-        #deleting the old rook from the board
-        board[old_x][old_y]="   ".colorize(background: color_piece(old_x,old_y))
-    end
-
-    def pieces
-        @pieces.each do |items|
-            puts items.join
-        end
-    end
-
+  end
 end
