9b05bee4dccce65dbf0aed8bb415f41a33316833
benxta4424 <benxtaben@gmail.com>
2024-09-11 19:35:02 +0100

    added a Piece superclass that takes care of adding the pieces on the board and keeping track of them

---
+++ lib/piece.rb
@@ -0,0 +1,12 @@
+class Piece
+    attr_accessor :color,:symbol
+
+    def initialize(col)
+        @color=col
+        @symbol=set_symbol
+    end
+
+    def check_color
+        @color=="white" ? @symbol : @symbol
+    end
+end
