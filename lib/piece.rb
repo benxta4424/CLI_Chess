8c5c5e895c1fd4b2d4e448c6a98c0c480ac536d5
benxta4424 <benxtaben@gmail.com>
2024-09-13 22:21:31 +0100

    adding the move_front method to the piece class as it supports the movement of every piece by doing this,we also respect the DRY principle

--- lib/piece.rb
+++ lib/piece.rb
@@ -7,6 +7,16 @@ class Piece
   end
 
   def check_color
-    @color = "white" ? @symbol : @symbol
+    @color == "white" ? @symbol : @symbol
+  end
+
+  def move_front(board, old_position, new_position)
+    old_x = old_position[0]
+    old_y = old_position[1]
+
+    new_x = new_position[0]
+    new_y = new_position[1]
+
+    board[new_x][new_y] = board[old_x][old_y]
   end
 end
