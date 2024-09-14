6e5eac3e0b6cd28b9709c0ae5b70e441e5321403
benxta4424 <benxtaben@gmail.com>
2024-09-15 00:04:08 +0100

    clear @legal_and_possible_moves in order to avoid traces of old possible movements

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -47,6 +47,9 @@ class Bishop < Piece
   end
 
   def possible_moves(pieces, current_x, current_y)
+
+    @possible_and_legal_positions.clear
+    
     starting_piece = pieces[current_x][current_y]
 
     get_possible_moves(pieces,current_x,current_y,1,1,starting_piece)  
@@ -55,5 +58,6 @@ class Bishop < Piece
     get_possible_moves(pieces,current_x,current_y,-1,-1,starting_piece) 
 
     @possible_and_legal_positions
+
   end
 end
