6e81fd98817d4fa3741bde36c6c86dd6eec8a016
benxta4424 <benxtaben@gmail.com>
2024-09-14 23:59:18 +0100

    using the previous method to gather all the possible moves for a rook

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -47,6 +47,13 @@ class Bishop < Piece
   end
 
   def possible_moves(pieces, current_x, current_y)
-    
+    starting_piece = pieces[current_x][current_y]
+
+    get_possible_moves(pieces,current_x,current_y,1,1,starting_piece)  
+    get_possible_moves(pieces,current_x,current_y,1,-1,starting_piece) 
+    get_possible_moves(pieces,current_x,current_y,-1,1,starting_piece) 
+    get_possible_moves(pieces,current_x,current_y,-1,-1,starting_piece) 
+
+    @possible_and_legal_positions
   end
 end
