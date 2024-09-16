63d47a265dcac1edc6989e06c006c887c8db3e1a
benxta4424 <benxtaben@gmail.com>
2024-09-16 01:55:28 +0100

    2 main movements: bishops and rook 8 possible cases: 4*bishop & 4*rook

--- lib/queen.rb
+++ lib/queen.rb
@@ -6,4 +6,21 @@ class Queen < Piece
     def set_symbol
         self.color == "white" ? " \u2655 " : " \u265B "
     end
+
+
+    def possible_moves(pieces,current_x,current_y)
+        @possible_and_legal_positions.clear
+
+        current_piece=pieces[current_x][current_y]
+
+        get_possible_moves(pieces,current_x,current_y,0,1,current_piece)
+        get_possible_moves(pieces,current_x,current_y,0,-1,current_piece)
+        get_possible_moves(pieces,current_x,current_y,1,0,current_piece)
+        get_possible_moves(pieces,current_x,current_y,-1,0,current_piece)
+        get_possible_moves(pieces,current_x,current_y,1,1,current_piece)
+        get_possible_moves(pieces,current_x,current_y,-1,1,current_piece)
+        get_possible_moves(pieces,current_x,current_y,1,-1,current_piece)
+        get_possible_moves(pieces,current_x,current_y,-1,-1,current_piece)
+        @possible_and_legal_positions
+    end
 end
