bc9ac65f67f3045dabae7641063c68385fdf19ec
benxta4424 <benxtaben@gmail.com>
2024-09-16 01:33:44 +0100

    removed the get_possible moves method and put in piece since it can be used for both rook and bishop

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -15,37 +15,6 @@ class Bishop < Piece
     x.between?(0, 7) && y.between?(0, 7)
   end
 
-  def get_possible_moves(pieces,current_x,current_y,x_case,y_case,starting_piece)
-    starting_piece=pieces[current_x][current_y]
-
-    x_axis = current_x+x_case
-    y_axis = current_y+y_case
-
-    while is_legal?(x_axis,y_axis)
-      current_piece = pieces[x_axis][y_axis]
-
-      if !current_piece.nil?
-
-        if current_piece.color != starting_piece.color && is_legal?(x_axis, y_axis)
-          @possible_and_legal_positions << [x_axis, y_axis]
-          break
-        elsif current_piece.color == starting_piece.color
-          @possible_and_legal_positions << [x_axis - x_case, y_axis - y_case]
-          break
-        end
-
-      elsif x_axis==7 || y_axis==7 || x_axis==0 || y_axis==0
-        @possible_and_legal_positions << [x_axis, y_axis]
-        break
-      end
-
-      x_axis += x_case
-      y_axis += y_case
-    end
-
-    @possible_and_legal_positions
-  end
-
   def possible_moves(pieces, current_x, current_y)
 
     @possible_and_legal_positions.clear
