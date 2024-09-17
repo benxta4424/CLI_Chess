7e68394e8cf13fac366785a40e206d726ba49fee
benxta4424 <benxtaben@gmail.com>
2024-09-17 22:33:37 +0100

    when the pawn reaches the end it can become any new piece of the same color except the king

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -7,67 +7,72 @@ class Pawn < Piece
     color == "white" ? " \u2659 " : " \u265F "
   end
 
-  def get_black_moves(pieces,current_x,current_y,starting_piece)
-    if starting_piece.color=="black"
+  def get_black_moves(pieces, current_x, current_y, starting_piece)
+    return unless starting_piece.color == "black"
 
-      current_piece=pieces[current_x+1][current_y]
+    current_piece = pieces[current_x + 1][current_y]
 
-      if current_piece.nil? && is_legal?(current_x+1,current_y)
-        @possible_and_legal_positions<<[current_x+1,current_y]
+    if current_piece.nil? && is_legal?(current_x + 1, current_y)
+      @possible_and_legal_positions << [current_x + 1, current_y]
 
-        if current_x == 1 && pieces[current_x+2][current_y].nil? && is_legal?(current_x+2, current_y)
-          @possible_and_legal_positions << [current_x+2, current_y]
-        end
-
-      end
-
-      if is_legal?(current_x+1,current_y+1) && !pieces[current_x+1][current_y+1].nil? && pieces[current_x+1][current_y+1].color!="black"
-        @possible_and_legal_positions<<[current_x+1,current_y+1]
-      end
-
-      if is_legal?(current_x+1,current_y-1) && !pieces[current_x+1][current_y-1].nil? && pieces[current_x+1][current_y-1].color!="black"
-        @possible_and_legal_positions<<[current_x+1,current_y-1]
+      if current_x == 1 && pieces[current_x + 2][current_y].nil? && is_legal?(current_x + 2, current_y)
+        @possible_and_legal_positions << [current_x + 2, current_y]
       end
 
     end
 
+    if is_legal?(current_x + 1,
+                 current_y + 1) && !pieces[current_x + 1][current_y + 1].nil? && pieces[current_x + 1][current_y + 1].color != "black"
+      @possible_and_legal_positions << [current_x + 1, current_y + 1]
+    end
+
+    if is_legal?(current_x + 1,
+                 current_y - 1) && !pieces[current_x + 1][current_y - 1].nil? && pieces[current_x + 1][current_y - 1].color != "black"
+      @possible_and_legal_positions << [current_x + 1, current_y - 1]
+    end
   end
 
-  def get_white_moves(pieces,current_x,current_y,starting_piece)
-    if starting_piece.color=="white"
+  def get_white_moves(pieces, current_x, current_y, starting_piece)
+    return unless starting_piece.color == "white"
 
-      current_piece=pieces[current_x-1][current_y]
+    current_piece = pieces[current_x - 1][current_y]
 
-      if current_piece.nil? && is_legal?(current_x-1,current_y)
-        @possible_and_legal_positions<<[current_x-1,current_y]
+    if current_piece.nil? && is_legal?(current_x - 1, current_y)
+      @possible_and_legal_positions << [current_x - 1, current_y]
 
-        if current_x == 6 && pieces[current_x-2][current_y].nil? && is_legal?(current_x-2, current_y)
-          @possible_and_legal_positions << [current_x-2, current_y]
-        end
-
-      end
-
-      if is_legal?(current_x-1,current_y+1) && !pieces[current_x-1][current_y+1].nil? && pieces[current_x-1][current_y+1].color!="white"
-        @possible_and_legal_positions<<[current_x-1,current_y+1]
-      end
-
-      if is_legal?(current_x-1,current_y-1) && !pieces[current_x-1][current_y-1].nil? && pieces[current_x-1][current_y-1].color!="white"
-        @possible_and_legal_positions<<[current_x-1,current_y-1]
+      if current_x == 6 && pieces[current_x - 2][current_y].nil? && is_legal?(current_x - 2, current_y)
+        @possible_and_legal_positions << [current_x - 2, current_y]
       end
 
     end
 
+    if is_legal?(current_x - 1,
+                 current_y + 1) && !pieces[current_x - 1][current_y + 1].nil? && pieces[current_x - 1][current_y + 1].color != "white"
+      @possible_and_legal_positions << [current_x - 1, current_y + 1]
+    end
+
+    if is_legal?(current_x - 1,
+                 current_y - 1) && !pieces[current_x - 1][current_y - 1].nil? && pieces[current_x - 1][current_y - 1].color != "white"
+      @possible_and_legal_positions << [current_x - 1, current_y - 1]
+    end
   end
 
-  def possible_moves(pieces,current_x,current_y)
+  def possible_moves(pieces, current_x, current_y)
     @possible_and_legal_positions.clear
 
-    current_piece=pieces[current_x][current_y]
+    current_piece = pieces[current_x][current_y]
 
-    get_black_moves(pieces,current_x,current_y,current_piece)
-    get_white_moves(pieces,current_x,current_y,current_piece)
+    get_black_moves(pieces, current_x, current_y, current_piece)
+    get_white_moves(pieces, current_x, current_y, current_piece)
 
     @possible_and_legal_positions
   end
 
+  def change_piece(current_x,current_y,new_piece_class)
+    if current_x==0
+      @possible_new_piece<<[new_piece_class,"white",current_x,current_y]
+    elsif current_x==7
+      @possible_new_piece<<[new_piece_class,"black",current_x,current_y]
+    end
+  end
 end
