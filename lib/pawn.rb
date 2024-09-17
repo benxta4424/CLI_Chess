6bc9f1580622e96fd2feaed202c14425ec1db5cf
benxta4424 <benxtaben@gmail.com>
2024-09-17 15:12:11 +0100

    hardcoded moves for white pawn

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -28,12 +28,34 @@ class Pawn < Piece
 
   end
 
+  def get_white_moves(pieces,current_x,current_y,starting_piece)
+    if starting_piece.color=="white"
+
+      current_piece=pieces[current_x-1][current_y]
+
+      if current_piece.nil? && is_legal?(current_x-1,current_y)
+        @possible_and_legal_positions<<[current_x-1,current_y]
+      end
+
+      if is_legal?(current_x-1,current_y+1) && !pieces[current_x-1][current_y+1].nil? && pieces[current_x-1][current_y+1].color!="white"
+        @possible_and_legal_positions<<[current_x-1,current_y+1]
+      end
+
+      if is_legal?(current_x-1,current_y-1) && !pieces[current_x-1][current_y-1].nil? && pieces[current_x-1][current_y-1].color!="white"
+        @possible_and_legal_positions<<[current_x-1,current_y-1]
+      end
+
+    end
+
+  end
+
   def possible_moves(pieces,current_x,current_y)
     @possible_and_legal_positions.clear
 
     current_piece=pieces[current_x][current_y]
 
     get_black_moves(pieces,current_x,current_y,current_piece)
+    get_white_moves(pieces,current_x,current_y,current_piece)
 
     @possible_and_legal_positions
   end
