60e47106c27d25f2aaf339b4e3b788b91e6c395b
benxta4424 <benxtaben@gmail.com>
2024-09-17 22:28:50 +0100

    added logic for first time movements for the pawns (they can move two squares for the first movement)

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -14,6 +14,11 @@ class Pawn < Piece
 
       if current_piece.nil? && is_legal?(current_x+1,current_y)
         @possible_and_legal_positions<<[current_x+1,current_y]
+
+        if current_x == 1 && pieces[current_x+2][current_y].nil? && is_legal?(current_x+2, current_y)
+          @possible_and_legal_positions << [current_x+2, current_y]
+        end
+
       end
 
       if is_legal?(current_x+1,current_y+1) && !pieces[current_x+1][current_y+1].nil? && pieces[current_x+1][current_y+1].color!="black"
@@ -35,6 +40,11 @@ class Pawn < Piece
 
       if current_piece.nil? && is_legal?(current_x-1,current_y)
         @possible_and_legal_positions<<[current_x-1,current_y]
+
+        if current_x == 6 && pieces[current_x-2][current_y].nil? && is_legal?(current_x-2, current_y)
+          @possible_and_legal_positions << [current_x-2, current_y]
+        end
+
       end
 
       if is_legal?(current_x-1,current_y+1) && !pieces[current_x-1][current_y+1].nil? && pieces[current_x-1][current_y+1].color!="white"
